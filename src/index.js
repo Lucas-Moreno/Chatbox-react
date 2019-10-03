fae7f2fdc13c16458e6ab596def07f6d599d9dfe
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2019-10-03 15:58:52 +0200

    chatbox

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import Connexion from './components/Connexion'
+import NotFound from './components/NotFound'
+import * as serviceWorker from './serviceWorker'
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+const Root = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Connexion} />
+      <Route path='/pseudo/:pseudo' component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
+
+serviceWorker.unregister()
